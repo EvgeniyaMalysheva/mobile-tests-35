a397bbc92080013bf58f5508cbf579d8d1bceadb
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-07-16 13:48:33 +0300

    video ok

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -18,23 +18,40 @@ public class BrowserstackDriver implements WebDriverProvider {
         MutableCapabilities caps = new MutableCapabilities();
 
         // Set your access credentials
-        caps.setCapability("browserstack.user", "bsuser_sLVSk3");
-        caps.setCapability("browserstack.key", "rZ3tApfRNDyqQViByc5f");
+//        caps.setCapability("browserstack.user", "bsuser_sLVSk3");
+//        caps.setCapability("browserstack.key", "rZ3tApfRNDyqQViByc5f");
 
         // Set URL of the application under test
-        caps.setCapability("app", "bs://sample.app");
+//        caps.setCapability("app", "bs://sample.app");
 
         // Specify device and os_version for testing
-//        caps.setCapability("device", "Google Pixel 3");
-        caps.setCapability("deviceName", "Google Pixel 3");
+//        caps.setCapability("device", "Google Pixel 6");
+//        caps.setCapability("deviceName", "Google Pixel 6");
 
-//        caps.setCapability("os_version", "9.0");
-        caps.setCapability("platformVersion", "9.0");
+//        caps.setCapability("os_version", "12.0");
+//        caps.setCapability("platformVersion", "12.0");
 
-        // Set other BrowserStack capabilities
-        caps.setCapability("project", "First Java Project");
-        caps.setCapability("build", "browserstack-build-1");
-        caps.setCapability("name", "first_test");
+        caps.setCapability("app", "bs://sample.app"); // Замените на реальный app ID
+        caps.setCapability("deviceName", "Samsung Galaxy S22 Ultra");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "12.0");
+        caps.setCapability("browserstackLocal", true);
+
+        MutableCapabilities bstackOptions = new MutableCapabilities();
+        bstackOptions.setCapability("userName", "bsuser_sLVSk3");
+        bstackOptions.setCapability("accessKey", "rZ3tApfRNDyqQViByc5f");
+        bstackOptions.setCapability("projectName", "BrowserStack Sample");
+        bstackOptions.setCapability("buildName", "browserstack-build-1");
+        bstackOptions.setCapability("sessionName", "first_test");
+        bstackOptions.setCapability("video", true);
+
+        caps.setCapability("bstack:options", bstackOptions);
+
+//
+//        // Set other BrowserStack capabilities
+//        caps.setCapability("project", "First Java Project");
+//        caps.setCapability("build", "browserstack-build-1");
+//        caps.setCapability("name", "first_test");
 
         // Initialise the remote Webdriver using BrowserStack remote URL
         // and desired capabilities defined above
--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -2,7 +2,6 @@ package helpers;
 
 import static io.restassured.RestAssured.given;
 
-
 public class Browserstack {
 
     // curl -u "qaguru_ti9G5S:5yrxu4nFTKkRExUAhqxh" -X GET "https://api.browserstack.com/app-automate/sessions/0359d759d2aaa4f46401dac46bd281b6d9b24943.json"
@@ -12,7 +11,7 @@ public class Browserstack {
         String url = String.format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
-                .auth().basic("qaguru_ti9G5S", "5yrxu4nFTKkRExUAhqxh")
+                .auth().basic("bsuser_sLVSk3", "rZ3tApfRNDyqQViByc5f")
                 .get(url)
                 .then()
                 .log().status()
